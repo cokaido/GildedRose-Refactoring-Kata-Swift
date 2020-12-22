6fc8c8ea5437f285a66e94ae89ed44f1fce93501
Guillem Fernandez <hello@guillem.cat>
2020-12-22 18:47:47 +0100

    Applyed lift up conditionals refactoring

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -4,57 +4,65 @@ public class GildedRose {
     public init(items:[Item]) {
         self.items = items
     }
+        
+
+    fileprivate func doUpdateQuality(_ item: Item) {
+        switch item.name {
+        case "Aged Brie":
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+            
+            item.sellIn = item.sellIn - 1
+            
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        case "Backstage passes to a TAFKAL80ETC concert":
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+                
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+                
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+            
+            item.sellIn = item.sellIn - 1
+            
+            if (item.sellIn < 0) {
+                item.quality = item.quality - item.quality
+            }
+        case "Sulfuras, Hand of Ragnaros":
+            break
+        default:
+            if (item.quality > 0) {
+                item.quality = item.quality - 1
+            }
+            
+            item.sellIn = item.sellIn - 1
+            
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1
+                }
+            }
+        }
+        
+    }
     
     public func updateQuality() {
         for i in 0..<items.count {
-            if (items[i].name != "Aged Brie"
-                    && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+            doUpdateQuality(items[i])
         }
     }
 }
